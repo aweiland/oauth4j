b43851bf2fbe00604ee9a5e4c7d9821d1fdb5239
aweiland <aweiland@gmail.com>
2016-07-03 17:14:46 -0400

    skeleton

---
+++ src/main/java/io/github/aweiland/Oauth4jApplication.java
@@ -0,0 +1,12 @@
+package io.github.aweiland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth4jApplication.class, args);
+	}
+}
